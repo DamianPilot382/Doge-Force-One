825a86ddfae5f2bba64e96adb420319191229c73
Damian Ugalde <damianugalde@gmail.com>
2023-11-16 13:31:21 -0800

    Add Comments

--- Main.py
+++ Main.py
@@ -13,48 +13,54 @@ import adafruit_mpl3115a2 # Altitude
 # Sensor Initialization
 i2c = board.I2C()
 
+# Temperature
 tempSensor = adafruit_mcp9808.MCP9808(i2c)
 
+# Altitude
 altSensor = adafruit_mpl3115a2.MPL3115A2(i2c)
 altSensor.sealevel_pressure = 1022.5
 
+# IMU
 imuSensor = adafruit_lsm9ds1.LSM9DS1_I2C(i2c)
 
-gps = adafruit_gps.GPS_GtopI2C(i2c, debug=False)
+# GPS
+gpsSensor = adafruit_gps.GPS_GtopI2C(i2c, debug=False)
 
-# Turn on the basic GGA and RMC info (what you typically want)
-gps.send_command(b"PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0")
-
+# Turn on the basic GGA and RMC info
+gpsSensor.send_command(b"PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0")
 # Set update rate to once a second (1hz)
-gps.send_command(b"PMTK220,1000")
+gpsSensor.send_command(b"PMTK220,1000")
 
+# Create a new numbered results CSV file
 inc = 0
 while os.path.exists('Results/Results_%s.csv' % inc):
     inc += 1
 
+# Open the CSV file
 csvFile = open('Results/Results_%s.csv' % inc, 'w')
 csvWriter = csv.writer(csvFile)
 
-headers = ['Time', 'Temperature', 'Pressure', 'Altitude',
-           'AccelX', 'AccelY', 'AccelZ',
-           'MagX', 'MagY', 'MagZ',
-           'GyroX', 'GyroY', 'GyroZ',
-           'GPSFix', 'GPSLat', 'GPSLong']
-
-csvWriter.writerow(headers)
+# Create headers for CSV File
+csvWriter.writerow(['Time',
+                    'Temperature', 'Pressure', 'Altitude',
+                    'AccelX', 'AccelY', 'AccelZ',
+                    'MagX', 'MagY', 'MagZ',
+                    'GyroX', 'GyroY', 'GyroZ',
+                    'GPSFix', 'GPSLat', 'GPSLong'])
 
 startTime = time.time()
 
-timestamp = time.time() - startTime
+# Loop forever or until interrupted
+while True:
 
-while timestamp < 60:
-
+    # Print separator
     print("=" * 40)
 
-    gps.update()
+    # Update the GPS (done twice per loop)
+    gpsSensor.update()
 
+    # Get the new timestamp and print it
     timestamp = time.time() - startTime
-
     print("Time: ", timestamp)
 
     # Temperature
@@ -68,7 +74,6 @@ while timestamp < 60:
     altitude = altSensor.altitude
     print("Altitude: {0:0.3f} meters".format(altitude))
     
-    
     # IMU
     print("IMU:")
     accel_x, accel_y, accel_z = imuSensor.acceleration
@@ -78,41 +83,46 @@ while timestamp < 60:
     print("    Magnetometer (gauss): ({0:0.3f},{1:0.3f},{2:0.3f})".format(mag_x, mag_y, mag_z))
     print("    Gyroscope (rad/sec): ({0:0.3f},{1:0.3f},{2:0.3f})".format(gyro_x, gyro_y, gyro_z))
     
+    # GPS
     print("GPS:")
-    if gps.has_fix:
-        lat = gps.latitude
-        long = gps.longitude
-        # Print out details about the fix like location, date, etc.
+
+    # Check if the GPS has a fix
+    if gpsSensor.has_fix:
+        lat = gpsSensor.latitude
+        long = gpsSensor.longitude
         print("    Latitude: {0:.6f} degrees".format(lat))
         print("    Longitude: {0:.6f} degrees".format(long))
-        print("    Fix quality: {}".format(gps.fix_quality))
-        # Some attributes beyond latitude, longitude and timestamp are optional
-        # and might not be present.  Check if they're None before trying to use!
-        if gps.satellites is not None:
-            print("# satellites: {}".format(gps.satellites))
-        if gps.altitude_m is not None:
-            print("Altitude: {} meters".format(gps.altitude_m))
-        if gps.speed_knots is not None:
-            print("Speed: {} knots".format(gps.speed_knots))
-        if gps.track_angle_deg is not None:
-            print("Track angle: {} degrees".format(gps.track_angle_deg))
-        if gps.horizontal_dilution is not None:
-            print("Horizontal dilution: {}".format(gps.horizontal_dilution))
-        if gps.height_geoid is not None:
-            print("Height geoid: {} meters".format(gps.height_geoid))
+        print("    Fix quality: {}".format(gpsSensor.fix_quality))
 
+        # Print extra information
+        if gpsSensor.satellites is not None:
+            print("# satellites: {}".format(gpsSensor.satellites))
+        if gpsSensor.altitude_m is not None:
+            print("Altitude: {} meters".format(gpsSensor.altitude_m))
+        if gpsSensor.speed_knots is not None:
+            print("Speed: {} knots".format(gpsSensor.speed_knots))
+        if gpsSensor.track_angle_deg is not None:
+            print("Track angle: {} degrees".format(gpsSensor.track_angle_deg))
+        if gpsSensor.horizontal_dilution is not None:
+            print("Horizontal dilution: {}".format(gpsSensor.horizontal_dilution))
+        if gpsSensor.height_geoid is not None:
+            print("Height geoid: {} meters".format(gpsSensor.height_geoid))
+    
+    # If no GPS fix
     else:
         print("    No fix")
         lat = 0
         long = 0
 
+    # Update the GPS Sensor again
+    gpsSensor.update()
 
-    gps.update()
-
+    # Write the results for this iteration to the CSV file
     csvWriter.writerow([timestamp, tempC, pressure, altitude,
                        accel_x, accel_y, accel_z,
                        mag_x, mag_y, mag_z,
                        gyro_x, gyro_y, gyro_z,
-                       gps.has_fix, lat, long])
+                       gpsSensor.has_fix, lat, long])
 
+    # Sleep before next loop
     time.sleep(0.5)
